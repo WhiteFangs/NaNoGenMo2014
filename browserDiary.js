e05b694ab81d2b54b5609f4d2c91f1037ce14b25
WhiteFangs <white-fangs@hotmail.fr>
2014-11-30 19:56:02 +0200

    Random beginning of entries, language handeld / Fixes #3

--- browserDiary.js
+++ browserDiary.js
@@ -64,8 +64,29 @@ function novelLoop(count, data){
   }
 }
 
+var contentIntro = ['Today I visited', "Oh hey, look where I went", "I spent so much time searching for this, and I found it here", "Something caught my eye on this page today",
+"I guess it was meant to be, I looked at this page", "You won't believe what I read here", "It's been so long I wanted to read such a thing, found it here",
+"Gone here today", "Do I really need to talk about this? Ok, I went here", "I didn't imagine I could find something like this, but I looked here",
+"There's probably something that went wrong, but I arrived here today", "See, navigating randomly got me here", "I swear I don't know how I got here",
+"Let's take a look at this", "Look what I found", "Not sure if it's interesting but I went here today"];
+
+var langIntro = ['Oh, this page is in ', "Hm, that's interesting... They wrote in ", "At least I'm learning ", "I like unsual pages, like this one speaking ",
+ "Cool, another language ! It's ", "Let's travel by the text, let's speak ", "Different languages, different minds, let's see how they think in ",
+ "I'm not afraid to be faced with a text I can't understand, like this one in ", "I wanted to see abroad, so I looked at a page in ",
+ "I took some kind of teleporter because I wanted to read ", "I already know Javascript, I want to learn "];
+
+var languages = ['ru', 'de', 'fr', 'ja', 'es', 'zh', 'pt', 'it', 'pl', 'nl', 'tr'];
+
+var langToText = {'ru' : 'russian, да да.', 'de' : 'german, ja ja.', 'fr' : 'french, oui oui.', 'ja' : 'japanese, はい はい.', 'zh' : 'chinese, 是的 是的.',
+ 'es' : 'spanish, si si.', 'pt' : 'portugese, sim sim.', 'it' : 'italian, si si.', 'pl' : 'polish, tak tak.', 'tr' : 'turkish, evet evet.', 'nl' : 'dutch, ja ja.'  };
+
+
 function displayContent(data){
-  $("#" + data.unixdate).append('Today, I visited <a href="'+ data.url + '">' + data.url + "</a>.<br><br>");
+  $("#" + data.unixdate).append(contentIntro[Math.floor(Math.random()*contentIntro.length)] + ' <a href="'+ data.url + '">' + data.url + "</a>.<br><br>");
+  var lang  = data.lang.slice(0, 2);
+  if(languages.indexOf(lang) > -1){
+    $('#' + data.unixdate).append(langIntro[Math.floor(Math.random()*langIntro.length)] + langToText[lang] + "<br><br>");
+  }
   if(data.paragraphs.length > 0){
     for(var i = 0; i < data.paragraphs.length; i++){
         $("#" + data.unixdate).append(data.paragraphs[i] + "<br><br>");
@@ -119,9 +140,10 @@ function displayBrowserInfo(infos, unix, novelTitle){
     }
   }
   var cookies = (navigator.cookieEnabled) ? "Oh, and I like cookies." : "Oh, and I don't like cookies.";
+  var infosLocal = (infos.city.length > 0) ? infos.city + ', ' + infos.country_name : infos.country_name;
 
   $("#" + unix).append("My name is " + navigator.appName + ' ' + browserName + ", I'm a web browser and this is my personal diary. <br>Actually, my full name is " +
-  navigator.appVersion + ', but everybody calls me ' + browserName + '.<br>I live in a ' + OSName + ' running computer, in ' + infos.city + ', ' + infos.country_name +
+  navigator.appVersion + ', but everybody calls me ' + browserName + '.<br>I live in a ' + OSName + ' running computer, in ' + infosLocal +
   '.<br>My exact address is ' + infos.ip + '. <br>' + cookies + '<br><br>Ok, I presented myself. Let\'s see what can I talk about now. Hm, yes, I know... <b>' + novelTitle + '</b>.<br><br>');
 }
 
